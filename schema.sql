b60ed341eb4d95128705136cd6b845e8cae12d05
soumyakant-sahu <soumyakantsahu67@gmail.com>
2026-07-19 22:35:41 +0530

    Add members and book issues tables

--- schema.sql
+++ schema.sql
@@ -36,3 +36,38 @@ create table books(
 	check (available_copies >= 0),
     check(available_copies <= total_copies)
     );
+
+   -- Create table members
+CREATE TABLE members (
+    member_id INT AUTO_INCREMENT PRIMARY KEY,
+    first_name VARCHAR(50) NOT NULL,
+    last_name VARCHAR(50) NOT NULL,
+    email VARCHAR(100) UNIQUE NOT NULL,
+    phone VARCHAR(15) UNIQUE NOT NULL,
+    join_date DATE DEFAULT (CURRENT_DATE),
+    membership_status ENUM('ACTIVE', 'INACTIVE') DEFAULT 'ACTIVE'
+); 
+
+-- Create table book_issues
+CREATE TABLE book_issues (
+    issue_id INT AUTO_INCREMENT PRIMARY KEY,
+
+    book_id INT NOT NULL,
+    member_id INT NOT NULL,
+
+    issue_date DATE DEFAULT (CURRENT_DATE),
+    due_date DATE NOT NULL,
+    return_date DATE,
+
+    status ENUM('ISSUED', 'RETURNED', 'OVERDUE')
+        DEFAULT 'ISSUED',
+
+    CONSTRAINT fk_issue_book
+        FOREIGN KEY (book_id)
+        REFERENCES books(book_id),
+
+    CONSTRAINT fk_issue_member
+        FOREIGN KEY (member_id)
+        REFERENCES members(member_id)
+);
+
