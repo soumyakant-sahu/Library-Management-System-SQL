25f80bc7504a5a5edf9c0167e2a0144759891fb5
soumyakant-sahu <soumyakantsahu67@gmail.com>
2026-07-23 16:55:02 +0530

    Added Triggers

--- triggers.sql
+++ triggers.sql
@@ -1,8 +1,7 @@
 USE library_management_system;
 
--- ==========================================================
--- TRIGGERS
--- ==========================================================
+-- Trigger to decrease available copies after issuing a book
+
 DELIMITER $$
 
 CREATE TRIGGER trg_after_book_issue
@@ -11,13 +10,28 @@ FOR EACH ROW
 BEGIN
     UPDATE books
     SET available_copies = available_copies - 1
-    WHERE book_id = NEW.book_id;
+    WHERE book_id = NEW.book_id
+      AND available_copies > 0;
 END $$
 
 DELIMITER ;
-CALL IssueBook(
-2,
-4,
-CURDATE(),
-DATE_ADD(CURDATE(), INTERVAL 14 DAY)
-);
+
+-- Trigger to increase available copies after returning a book
+
+DELIMITER $$
+
+CREATE TRIGGER trg_after_book_return
+AFTER UPDATE ON book_issues
+FOR EACH ROW
+BEGIN
+    IF OLD.status <> 'RETURNED'
+       AND NEW.status = 'RETURNED' THEN
+
+        UPDATE books
+        SET available_copies = available_copies + 1
+        WHERE book_id = NEW.book_id;
+
+    END IF;
+END $$
+
+DELIMITER ;
